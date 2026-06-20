1513c660e3da12068d858cfb62ef202da943097d
Hardi <hardivirani2003@gmail.com>
2026-06-20 19:20:35 -0400

    vid : 87 - Sending Data & RequestBody

--- src/main/java/com/star/spring_boot_rest/JobRestController.java
+++ src/main/java/com/star/spring_boot_rest/JobRestController.java
@@ -29,4 +29,9 @@ public class JobRestController {
         return service.getJob(postId);
     }
 
+    @PostMapping("jobPost")
+    public void addJob(@RequestBody JobPost jobPost) {
+        service.addJob(jobPost);
+    }
+
 }
